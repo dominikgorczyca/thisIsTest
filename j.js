1702eee34514a9f7331b709a9cd9bed3a4b18756
Pr0grammingTeenager <73836513+dominikgorczyca@users.noreply.github.com>
2021-04-30 15:19:25 +0200

    Update j.js

--- j.js
+++ j.js
@@ -1,23 +1,11 @@
 const gameBoard = document.getElementById("game-board");
 const root = document.documentElement.style;
 
-
-const characters = [{
-    name: "yellow",
-    position: 658,
-    characterNode: undefined,
-}];
-
 let elements;
+let characterNode;
+let position = 658;
 
-const yellowSprite = {
-    "ArrowRight": 0,
-    "ArrowLeft": 32,
-    "ArrowUp": 64,
-    "ArrowDown": 96,
-}
 
-      
 startLevel();
 
 function startLevel() {
@@ -28,7 +16,7 @@ function makeLevel() {
     for (let i = 0; i < 868; i++) {
         const element = document.createElement("DIV");
         element.className = "wall"
-        element.style.backgroundPosition = `-220px 0`;
+        element.style.backgroundPosition = `-240px 0`;
 
         let character = document.createElement("DIV");
         character.className = "yellow";
@@ -37,11 +25,13 @@ function makeLevel() {
         gameBoard.append(element);
     }
     elements = Array.from(gameBoard.children);
-    characters[0].characterNode = elements[658].children[0];
+    characterNode = elements[658].children[0];
 }
+
+
 function characterMove() {
 
-    root.setProperty(`--yellow-sprite-y`, `-${yellowSprite["ArrowLeft"]}px`);
+    root.setProperty(`--yellow-sprite-y`, `-32px`);
 
     getTransition();
     changePosition();
@@ -55,17 +45,17 @@ function getTransition() {
         "ArrowLeft": "X(-20px)",
     }
 
-    characters[0].characterNode.style.transform = `translate${transitionMove["ArrowLeft"]}`;
+    characterNode.style.transform = `translate${transitionMove["ArrowLeft"]}`;
 }
 function changePosition() {
-    characters[0].characterNode.classList.add(`yellow-animation-move`);
+    characterNode.classList.add(`yellow-animation-move`);
         
     setTimeout(() => {
-        characters[0].characterNode.classList.remove(`yellow-animation-move`, `yellow-visible`);
-        characters[0].characterNode.style.transform = "";
-        characters[0].position = characters[0].position - 1;
-        characters[0].characterNode = elements[characters[0].position].children[0];
-        characters[0].characterNode.classList.add(`yellow-visible`);
+        characterNode.classList.remove(`yellow-animation-move`, `yellow-visible`);
+        characterNode.style.transform = "";
+        position = position - 1;
+        characterNode = elements[position].children[0];
+        characterNode.classList.add(`yellow-visible`);
         characterMove()
     }, 200)
 }
