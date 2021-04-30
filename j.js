097dfbba13746b51922cc8845d6da21ac44bbfd2
Pr0grammingTeenager <73836513+dominikgorczyca@users.noreply.github.com>
2021-04-30 15:27:39 +0200

    Update j.js

--- j.js
+++ j.js
@@ -4,13 +4,15 @@ const root = document.documentElement.style;
 let elements;
 let characterNode;
 let position = 658;
+
+
 makeLevel();
 
 function makeLevel() {
     for (let i = 0; i < 868; i++) {
         const element = document.createElement("DIV");
         element.className = "wall"
-        element.style.backgroundPosition = `-260px 0`;
+        element.style.backgroundPosition = `-240px 0`;
 
         let character = document.createElement("DIV");
         character.className = "yellow";
@@ -23,7 +25,10 @@ function makeLevel() {
     characterMove();
 }
 
+
 function characterMove() {
+    root.setProperty(`--yellow-sprite-y`, `-32px`);
+
     getTransition();
     changePosition();
 }
@@ -33,13 +38,14 @@ function getTransition() {
         "ArrowLeft": "X(-20px)",
     }
 
-    characterNode.style.transform = `translateX(-20px)`;
+    characterNode.style.transform = `translate${transitionMove["ArrowLeft"]}`;
 }
 function changePosition() {
     characterNode.classList.add(`yellow-animation-move`);
         
     setTimeout(() => {
         characterNode.classList.remove(`yellow-animation-move`, `yellow-visible`);
+        characterNode.style.transform = "";
         position = position - 1;
         characterNode = elements[position].children[0];
         characterNode.classList.add(`yellow-visible`);
