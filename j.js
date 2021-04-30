ff371ffabf8adcf20fbf2342183946b84c3bfd74
Pr0grammingTeenager <73836513+dominikgorczyca@users.noreply.github.com>
2021-04-30 15:56:33 +0200

    Update j.js

--- j.js
+++ j.js
@@ -11,7 +11,7 @@ function makeLevel() {
     for (let i = 0; i < 868; i++) {
         const element = document.createElement("DIV");
         element.className = "wall"
-        element.style.backgroundPosition = `0 0`;
+        element.style.backgroundPosition = `-20px 0`;
 
         let character = document.createElement("DIV");
         character.className = "yellow";
@@ -21,23 +21,20 @@ function makeLevel() {
     }
     elements = Array.from(gameBoard.children);
     characterNode = elements[658].children[0];
-    characterMove();
-}
-
-
-function characterMove() {
-    root.setProperty(`--yellow-sprite-y`, `-32px`);
-    characterNode.style.transform = `translateX(-20px)`;
     changePosition();
 }
+
 function changePosition() {
+    root.setProperty(`--yellow-sprite-y`, `-32px`);
+    characterNode.style.transform = `translateX(-20px)`;
+
     setTimeout(() => {
         characterNode.classList.remove(`yellow-visible`);
         characterNode.style.transform = "";
         position = position - 1;
         characterNode = elements[position].children[0];
         characterNode.classList.add(`yellow-visible`);
-        characterMove()
+        changePosition()
     }, 200)
 }
                        
